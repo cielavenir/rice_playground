0827bc1815f5f5989cc9852abd3001d2e1c1ead9
cielavenir <cielartisan@gmail.com>
2022-03-03 00:23:10 +0900

    add comment

--- sleeptest.rb
+++ sleeptest.rb
@@ -12,6 +12,22 @@ end
 
 2.times{|i|
     thread = Thread.start{f(i)}
-	5.times{sleep(0.1);puts('hello')}
+    5.times{sleep(0.1);puts('hello')}
     thread.join
 }
+
+__END__
+sleep start
+hello # main-thread's hello can be executed during sleep(3)
+hello
+hello
+hello
+hello
+sleep end
+sleep start
+hello # took 3 secs to show the first hello; RicePlayground::ToyClass.new.sleep(3) blocks execution
+sleep end
+hello
+hello
+hello
+hello
